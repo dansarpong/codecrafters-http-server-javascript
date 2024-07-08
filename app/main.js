269efe583148d614ffdd792b9e5036680622b650
dansarpong <dansarpong.me@gmail.com>
2024-07-08 03:05:02 +0000

    Respond with 200

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
   socket.on("close", () => {
     socket.end();
   });
