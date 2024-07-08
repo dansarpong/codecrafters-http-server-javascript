0df6d047068e5b8f91b04a76edae43ebc5bd9a24
dansarpong <dansarpong.me@gmail.com>
2024-07-08 21:32:39 +0000

    Fix

--- app/main.js
+++ app/main.js
@@ -2,11 +2,11 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const request = data.toString().split("\r\n")[0];
-    if (request.startsWith("GET / ")) {
+    const request = data.toString().split("\r\n");
+    if (request[0].startsWith("GET / ")) {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (request.startsWith("GET /echo/")) {
-      const content = request.split(" ")[1].split("/")[1];
+    } else if (request[0].startsWith("GET /echo/")) {
+      const content = request[0].split(" ")[1].split("/")[2];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
