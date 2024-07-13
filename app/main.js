c6917f30dd46c8af6bccdcc03760835da32a6e67
dansarpong <dansarpong.me@gmail.com>
2024-07-13 13:55:42 +0000

    Read Header

--- app/main.js
+++ app/main.js
@@ -3,14 +3,19 @@ const net = require("net");
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString().split("\r\n");
+
     if (request[0].startsWith("GET / ")) {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (request[0].startsWith("GET /echo/")) {
       const content = request[0].split(" ")[1].split("/")[2];
       socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+    } else if (request[0].startsWith("GET /user-agent ")) {
+      const content = request[2].split(" ")[1];
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
+
   });
   socket.on("close", () => {
     socket.end();
